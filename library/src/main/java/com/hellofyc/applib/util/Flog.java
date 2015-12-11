7717df25ad1f3254cb1043942885f72c16b7d9d7
fangyucun@126.com <fangyucun@126.com>
2015-12-11 20:21:26 +0800

    update!

--- library/src/main/java/com/hellofyc/applib/util/Flog.java
+++ library/src/main/java/com/hellofyc/applib/util/Flog.java
@@ -16,13 +16,7 @@
 
 package com.hellofyc.applib.util;
 
-import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.support.annotation.Nullable;
-import android.support.v4.util.ArrayMap;
-import android.text.TextUtils;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -30,11 +24,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 /**
  * Log Tool
@@ -42,380 +31,128 @@ import java.util.concurrent.Executors;
  *
  * @author Jason Fang
  */
-public final class Flog {
-	static final boolean DEBUG = false;
-	
-//	private static final String UPLOAD_URL = "";
-	
-	private static final int MSG_WHAT_WRITE = 1;
-	private static final int MSG_WHAT_UPLOAD = 2;
-	private static final int MSG_WHAT_UPLOAD_BY_FTP = 3;
-	
-	private static Flog mInstance;
-	private Context mContext;
-	private Executor mThreadPool = Executors.newFixedThreadPool(4);
-	private MainHandler mHandler = new MainHandler(Looper.getMainLooper());
-	private File mDefaultDir;
-	private File mCrashDir;
-	private String mDefaultFileName;
-	
-	private Flog(Context context) {
-		mContext = context;
-		try {
-			mDefaultDir = FileUtils.getDirectory(StorageUtils.getExternalStorageRootDir().toString() + "/" + 
-						StringUtils.getSimpleNameFromFullName(mContext.getPackageName()));
-		} catch (IOException e) {
-			mDefaultDir = context.getFilesDir();
-		}
-		mCrashDir = new File(context.getCacheDir(), "logs");
-		FileUtils.createDir(mCrashDir);
-		mDefaultFileName = "log-" + TimeUtils.getCurrentTime("yyyyMMdd_HHmmss") + ".log";
-	}
-	
-	public static Flog getInstance(Context context) {
-		if (mInstance == null) {
-			mInstance = new Flog(context);
-		}
-		return mInstance;
-	}
+public final class FLog {
+    private static final boolean ENABLE = true;
 
-	public static void i(String tag, String message) {
-		i(tag, message, false);
-	}
-	
-	public static void i(Object obj) {
-        String[] messages = StringUtils.divideString(String.valueOf(obj), 4000);
-        int size = messages.length;
-        for (int i=0; i<size; i++) {
-            if (i == 0) {
-                println(Log.INFO, getInvokerClassName(), messages[i]);
-            } else {
-                println(Log.INFO, getInvokerClassName(), "->" + messages[i]);
-            }
-        }
-	}
-	
-	public static void i(String tag, Object obj) {
-		println(Log.INFO, tag, String.valueOf(obj));
-	}
-	
-	public static void i(String tag, String format, Object... args) {
-		println(Log.INFO, tag, String.format(format, args));
-	}
+    public static final int VERBOSE      = Log.VERBOSE;
+    public static final int DEBUG        = Log.DEBUG;
+    public static final int INFO         = Log.INFO;
+    public static final int WARN         = Log.WARN;
+    public static final int ERROR        = Log.ERROR;
+    public static final int ASSERT       = Log.ASSERT;
+    public static final int JSON         = 8;
+    public static final int FILE         = 9;
 
-    public static void w(Object text) {
-        println(Log.WARN, getInvokerClassName(), String.valueOf(text));
+    public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	
+    public static void i() {
+        i(null, "");
     }
-	
-	public static void e(Object obj) {
-		println(Log.ERROR, getInvokerClassName(), String.valueOf(obj));
-	}
-	
-	public static void e(Throwable tr) {
-		println(Log.ERROR, getInvokerClassName(), getStackTraceString(tr));
-	}
-	
-	public static void e(String tag, Throwable tr) {
-		println(Log.ERROR, tag, getStackTraceString(tr));
-	}
-	
-	public static void e(String tag, String msg, Throwable tr) {
-		println(Log.ERROR, tag, msg + "\n" + getStackTraceString(tr));
-	}
-	
-	public static void e(String tag, String format, Object... args) {
-		println(Log.ERROR, tag, String.format(format, args));
-	}
+
+    public static void i(Object textObject) {
+        i(null, String.valueOf(textObject));
+    }
+
+    public static void i(String tag, String text) {
+        printLog(INFO, new LogInfo(tag, text));
+    }
+
+    public static void e() {
+        e(null, "");
+    }
+
+    public static void e(Object textObject) {
+        e(null, String.valueOf(textObject));
+    }
+
+    public static void e(String tag, String text) {
+        printLog(ERROR, new LogInfo(tag, text));
+    }
 
     public static void json(String jsonText) {
-        int indent = 4;
-        if (TextUtils.isEmpty(jsonText)) {
-            i("json is null");
-            return;
+        printLog(JSON, new LogInfo(null, jsonText));
+    }
+
+    public static void file(String path, String text) {
+        printLog(FILE, new LogInfo(null, text, path));
+    }
+
+    @SuppressWarnings("PointlessBooleanExpression")
+    private static void printLog(int type, @NonNull  LogInfo logInfo) {
+        if (!ENABLE) return;
+
+        String[] wrapContent = getWrappedContent(logInfo.tag, logInfo.text);
+        logInfo.tag = wrapContent[0];
+        String prefix = wrapContent[1];
+        logInfo.text = wrapContent[2];
+
+        switch (type) {
+            case JSON:
+                printJson(logInfo.tag, prefix, logInfo.text);
+                break;
+            case FILE:
+                printFile(logInfo.path, logInfo.text);
+                break;
+            default:
+                String[] texts = StringUtils.divideString(prefix.concat(logInfo.text), 4000);
+                for (String splitedText : texts) {
+                    Log.println(type, logInfo.tag, splitedText);
+                }
+                break;
         }
+    }
+
+    private static String[] getWrappedContent(String tag, String text) {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        String className = stackTrace[5].getFileName();
+        String methodName = stackTrace[5].getMethodName();
+        int lineNumber = stackTrace[5].getLineNumber();
+        String methodNameShort = methodName.substring(0, 1).toUpperCase() + methodName.substring(1);
+        String textPrefix = ("[ (").concat(className).concat(":").concat(lineNumber + "").concat(")#").concat(methodNameShort).concat(" ] ");
+        return new String[]{tag == null ? className : tag, textPrefix, text == null ? "NULL" : text};
+    }
+
+    private static void printJson(String tag, String prefix, String jsonText) {
+        String message;
         try {
             if (jsonText.startsWith("{")) {
                 JSONObject jsonObject = new JSONObject(jsonText);
-                String msg = jsonObject.toString(indent);
-                i(msg);
+                message = jsonObject.toString(4);
             } else if (jsonText.startsWith("[")) {
                 JSONArray jsonArray = new JSONArray(jsonText);
-                String msg = jsonArray.toString(indent);
-                i(msg);
+                message = jsonArray.toString(4);
+            } else {
+                message = jsonText;
             }
         } catch (JSONException e) {
-            e(e);
+            message = jsonText;
+        }
+
+        message = prefix + LINE_SEPARATOR + message;
+        String[] lines = message.split(LINE_SEPARATOR);
+        for (String line : lines) {
+            Log.println(INFO, tag, "║ " + line);
         }
     }
-	
-	public static String getCurrentMethodName() {
-		return Thread.currentThread().getStackTrace()[2].getMethodName();
-	}
-	
-	public static void p(Object o) {
-		System.out.println(o);
-	}
-	
-	public static void p(String TAG, Object obj) {
-		System.out.println(TextUtils.isEmpty(TAG) ? obj : TAG + ":" + obj);
-	}
-	
-	public static void err(Object o) {
-		System.err.println(o);
-	}
-	
-	public static String getStackTraceString(Throwable tr) {
-		return Log.getStackTraceString(tr);
-	}
-	
-	public static void println(int priority, String tag, String msg) {
-		Log.println(priority, tag, msg);
-	}
-	
-	public static String getInvokerClassName() {
-		StackTraceElement[] elements = (new Throwable()).getStackTrace();
-		return StringUtils.getSimpleNameFromFullName(elements[2].getClassName());
-	}
-	
-	public static String getInvokerMethodName() {
-		StackTraceElement[] elements = (new Throwable()).getStackTrace();
-		return elements[2].getMethodName();
-	}
-	
-	public void logToFile(String text) {
-		logToFile(text, null);
-	}
-	
-	public void logToFile(String text, OnWriteListener listener) {
-		logToFile(mDefaultDir, mDefaultFileName, text, listener);
-	}
-	
-	public void logToFile(File dir, String fileName, String text, OnWriteListener listener) {
-		mThreadPool.execute(new WriteTask(dir, fileName, text, listener));
-	}
-	
-//	public void uploadCrashLogByFtp(OnUploadListener listener) {
-//		mThreadPool.execute(new UploadCrashLogByFtpTask(listener));
-//	}
-	
-	/**
-	 * 上传日志
-	 * @param id 可以指定上传的目录ID
-	 * @param fileList 文件列表 为空时取默认
-	 * @param listener 回调
-	 */
-	public void uploadLog(@Nullable String id, @Nullable List<File> fileList, @Nullable OnUploadListener listener) {
-		mThreadPool.execute(new UploadTask(id, fileList, listener));
-	}
-	
-	static class MainHandler extends Handler {
 
-		public MainHandler(Looper looper) {
-			super(looper);
-		}
-		
-		@Override
-		public void handleMessage(Message msg) {
-			HandlerResult result = (HandlerResult) msg.obj;
-			if (msg.what == MSG_WHAT_WRITE) {
-				OnWriteListener onWriteListener = null;
-				if (result.mOnOpListener instanceof OnWriteListener) {
-					onWriteListener = (OnWriteListener)result.mOnOpListener;
-				}
-				
-				if (result.mIsSuccess) {
-					if (onWriteListener != null) {
-						onWriteListener.onWriteSuccess();
-					}
-				} else {
-					if (onWriteListener != null) {
-						onWriteListener.onWriteFailure(msg.what);
-					}
-				}
-			} else if (msg.what == MSG_WHAT_UPLOAD || msg.what == MSG_WHAT_UPLOAD_BY_FTP) {
-				OnUploadListener onUploadListener = null;
-				if (result.mOnOpListener instanceof OnUploadListener) {
-					onUploadListener = (OnUploadListener)result.mOnOpListener;
-				}
-				
-				if (onUploadListener != null) {
-					onUploadListener.onUploadResult(result.mResultMap);
-				}
-			}
-			super.handleMessage(msg);
-		}
-		
-	}
-	
-	static class HandlerResult {
-		OnOpListener mOnOpListener;
-		boolean mIsSuccess;
-		Map<String, Boolean> mResultMap;
-		
-		public HandlerResult(OnOpListener listener) {
-			mOnOpListener = listener;
-		}
-		
-		public void setResult(boolean isSuccess) {
-			mIsSuccess = isSuccess;
-		}
-		
-		public void setResultMap(Map<String, Boolean> map) {
-			mResultMap = map;
-		}
-		
-	}
-	
-	/**
-	 * 日志线程
-	 * @author Yucun
-	 *
-	 */
-	class WriteTask implements Runnable {
+    private static void printFile(String path, String text) {
+        File file = new File(path);
+        FileUtils.writeString(text, file.getParentFile(), file.getName());
+    }
 
-		private File mTargetDir;
-		private String mFileName;
-		private String mText;
-		private OnWriteListener mOnWriteListener;
-		
-		public WriteTask(File dir, String fileName, String text, OnWriteListener listener) {
-			mTargetDir = dir;
-			mFileName = fileName;
-			mText = text;
-			mOnWriteListener = listener;
-		}
-		
-		@Override
-		public void run() {
-			if (mContext == null) {
-				Flog.e("mContext can not be null!");
-				return;
-			}
-			
-			Message msg = mHandler.obtainMessage(MSG_WHAT_WRITE);
-			HandlerResult result = new HandlerResult(mOnWriteListener);
-			
-			if (StorageUtils.isExternalStorageAvailable(mContext)) {
-				File file = FileUtils.writeString(mText, mTargetDir, mFileName);
-				if (file == null) {
-					result.setResult(false);
-				} else {
-					result.setResult(true);
-				}
-			} else {
-				result.setResult(false);
-			}
-			msg.obj = result;
-			
-			if (mOnWriteListener != null) {
-				mHandler.sendMessage(msg);
-			}
-		}
-		
-	}
+    static class LogInfo {
+        String tag;
+        String text;
+        String path;
 
-	/**
-	 * 上传线程
-	 * @author Yucun
-	 *
-	 */
-	class UploadTask implements Runnable {
+        public LogInfo(String tag, String text) {
+            this(tag, text, null);
+        }
 
-		private String mId;
-		private List<File> mFileList;
-		private OnUploadListener mOnUploadListener;
-		
-		public UploadTask(String id, List<File> fileList, OnUploadListener listener) {
-			mId = id;
-			mFileList = fileList;
-			mOnUploadListener = listener;
-		}
-		
-		@Override
-		public void run() {
-			if (mContext == null) {
-				Flog.e("mContext can not be null!");
-				return;
-			}
-			
-			Message msg = mHandler.obtainMessage(MSG_WHAT_UPLOAD);
-//			HandlerResult result = new HandlerResult(mOnUploadListener);
-			Map<String, String> params = new ArrayMap<>();
-			params.put("id", mId);
-			
-			if (CollectionUtils.isEmpty(mFileList)) {
-				mFileList = FileUtils.getSubFiles(mDefaultDir);
-			}
-			
-//			ArrayMap<String, Boolean> resultMap = new ArrayMap<String, Boolean>();
-//			for (File file : mFileList) {
-//				FIXME
-//				String resultString = HttpUtils.uploadFile(UPLOAD_URL, params, file);
-//				if (!TextUtils.isEmpty(resultString)) {
-//					FileUtils.deleteFile(file);
-//					resultMap.put(file.getPath(), true);
-//				} else {
-//					resultMap.put(file.getPath(), false);
-//				}
-//			}
-//			result.setResultMap(resultMap);
-//			msg.obj = result;
-//			
-			if (mOnUploadListener != null) {
-				mHandler.sendMessage(msg);
-			}
-		}
-		
-	}
-	
-//	class UploadCrashLogByFtpTask implements Runnable {
-//
-//		private OnUploadListener mOnUploadListener;
-//		
-//		public UploadCrashLogByFtpTask(OnUploadListener listener) {
-//			mOnUploadListener = listener;
-//		}
-//		
-//		@Override
-//		public void run() {
-//			if (mContext == null) {
-//				Flog.e("mContext can not be null!");
-//				return;
-//			}
-//			
-//			Message msg = mHandler.obtainMessage(MSG_WHAT_UPLOAD_BY_FTP);
-//			HandlerResult result = new HandlerResult(mOnUploadListener);
-//			
-//			Map<String, Boolean> resultMap = new ArrayMap<String, Boolean>();
-//			
-//			File[] files = mCrashDir.listFiles();
-//			if (files != null && files.length != 0) {
-//				resultMap = FtpUtils.upload(TimeUtils.getCurrentDate(), Arrays.asList(files));
-//			}
-//			
-//			for (Map.Entry<String, Boolean> entry : resultMap.entrySet()) {
-//				if (entry.getValue()) FileUtils.deleteFile(entry.getKey());
-//			}
-//			
-//			result.setResultMap(resultMap);
-//			msg.obj = result;
-//			
-//			if (mOnUploadListener != null) {
-//				mHandler.sendMessage(msg);
-//			}
-//		}
-//		
-//	}
-	
-	public interface OnWriteListener extends OnOpListener {
-		void onWriteSuccess();
-		void onWriteFailure(int errorCode);
-	}
-	
-	public interface OnUploadListener extends OnOpListener {
-		void onUploadResult(Map<String, Boolean> filePathMap);
-	}
-	
-	interface OnOpListener {
-	}
+        public LogInfo(String tag, String text, String path) {
+            this.tag = tag;
+            this.text = text;
+            this.path = path;
+        }
+    }
 }
