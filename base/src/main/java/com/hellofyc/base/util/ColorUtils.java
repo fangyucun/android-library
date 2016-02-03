b88c6a9c29dd1485748c9e7a8b8e2a3c46c05e73
fangyucun <fangyucun@360.cn>
2016-02-03 16:22:17 +0800

    Update

--- base/src/main/java/com/hellofyc/base/util/ColorUtils.java
+++ base/src/main/java/com/hellofyc/base/util/ColorUtils.java
@@ -31,4 +31,39 @@ public class ColorUtils {
         HSV[2] *= 0.8f;
         return Color.HSVToColor(HSV);
     }
+
+    public static String getColorString(int color) {
+        if (color < 0 || color > 255) {
+            throw new IllegalArgumentException("color cannot rather than 255 or less than 0!");
+        }
+        String hexString = Integer.toHexString(color);
+        return hexString.length() != 2 ? "0".concat(hexString) : hexString;
+    }
+
+    public static String getAlphaString(float opacity) {
+        return getColorString((int) (opacity * 255));
+    }
+
+    public static int getARGBColor(int alpha, int color) {
+        return Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color));
+    }
+
+    public static int getRGBColor(int color) {
+        return Color.rgb(Color.red(color), Color.green(color), Color.blue(color));
+    }
+
+    public static String getARGBColorString(int color) {
+        String alphaString = getColorString(Color.alpha(color));
+        String redString = getColorString(Color.red(color));
+        String greenString = getColorString(Color.green(color));
+        String blueString = getColorString(Color.blue(color));
+        return "#".concat(alphaString).concat(redString).concat(greenString).concat(blueString);
+    }
+
+    public static String getRGBColorString(int color) {
+        String redString = getColorString(Color.red(color));
+        String greenString = getColorString(Color.green(color));
+        String blueString = getColorString(Color.blue(color));
+        return "#".concat(redString).concat(greenString).concat(blueString);
+    }
 }
